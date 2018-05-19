073f9dd5fb5eafe7ea5b308b24040c6e89deac1a
KevinDelcourt <38816626+KevinDelcourt@users.noreply.github.com>
2018-05-19 20:36:07 +0200

    Correction page.php -> n'affiche pas la date de création de la page
    A faire: 
    
    Afficher une icone fa par catégorie qui s'afficherait en fond de l'article
    trouver des trucs en js à mettre fiddle
    un message de 'vous acceptez les cookies bordel'

--- page.php
+++ page.php
@@ -10,7 +10,7 @@
 
                         <div class="card-header">
                             <h2 class="display-4"><?php the_title(); ?></h2>
-                            <p class="font-weight-light"><?php the_date(); ?> </p>
+                            
                         </div>
 
                         <div class="card-body">
